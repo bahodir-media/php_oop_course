3cc76dc18fd7e493a7665982a9cd25f9c194fc5e
Bahodir Media <bahodir.media@gmail.com>
2025-03-22 23:18:12 +0500

    .git

--- 05/index.php
+++ 05/index.php
@@ -1,10 +1,26 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
 
-require_once 'classes/File.php';
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
 
-$file = new File(__DIR__ . '/file.txt');
-$file->write('Строка 1');
-$file->write('Строка 2');
-$file->write('Строка 3');
-$file->write('Строка 4');
-$file->write('Строка 5');
+<body>
+
+	<h1>Домашнее задание. Класс для работы с файлом</h1>
+	<?php
+
+	require_once 'classes/File.php';
+
+	$file = new File(__DIR__ . '/file.txt');
+	$file->write('Строка 1');
+	$file->write('Строка 2');
+	$file->write('Строка 3');
+	$file->write('Строка 4');
+	$file->write('Строка 5');
+	?>
+</body>
+
+</html>
