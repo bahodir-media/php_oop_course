4f67bba11094814315a2353312db871dafb85dda
Bahodir Media <bahodir.media@gmail.com>
2025-04-01 22:42:04 +0500

    Константы класса: Статические свойства и методы

--- 06/classes/Car.php
+++ 06/classes/Car.php
@@ -7,10 +7,10 @@ class Car
 	public $wheels;
 	public $speed;
 	public $brand;
-	
+
 	public const TEST_CAR = 'Prototype';
 	public const TEST_CAR_SPEED = 300;
-	
+
 	public static $countCar = 0;
 
 	public function __construct($color, $wheels = 4, $speed, $brand)
@@ -21,16 +21,17 @@ class Car
 		$this->brand = $brand;
 		self::$countCar++;
 	}
-	
+
 	public static function getCount()
 	{
 		return self::$countCar;
 	}
-	
-	public function test() {
+
+	public function test()
+	{
 		echo 'Тест метода класса<br>';
 	}
-	
+
 	public function getCarInfo()
 	{
 		return "<h3>О моем авто:</h3>
@@ -39,7 +40,7 @@ class Car
             Кол-во колес: {$this->wheels}<br>
             Скорость: {$this->speed}<br>";
 	}
-	
+
 	public function getPrototypeInfo()
 	{
 		return "<h3>О прототипе:</h3>
--- 06/index.php
+++ 06/index.php
@@ -18,20 +18,20 @@
 	}
 	echo Car::$countCar;
 	echo '<br>';
-	
+
 	$car1 = new Car('черный', 4, 180, 'volvo');
 	echo Car::$countCar;
 	echo '<br>';
 	$car2 = new Car('черный', 4, 200, 'bmw');
-	
+
 	echo Car::getCount();
-	
+
 	echo $car1->getCarInfo();
 	echo $car2->getCarInfo();
 	//echo $car1::Test(); // Ошибка, так как метод test() не статический depricated
 	
 	echo $car1->getPrototypeInfo();
-	
+
 	echo Car::TEST_CAR_SPEED; // 300 
 	
 	echo Car::class;
