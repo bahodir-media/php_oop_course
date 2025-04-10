4e20a02db94f04ba7995c6efdb9599c37dfb9fef
Bahodir Media <bahodir.media@gmail.com>
2025-04-10 20:52:13 +0500

    Трейты

---
+++ 14/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Премиум. Курс по ООП PHP</title>
+</head>
+
+<body>
+	<h1>Урок 14. Трейты</h1>
+	<?php
+
+	use core\interfaces\IGadget;
+	use app\{BookProduct, NoteBookProduct};
+
+	require_once __DIR__ . '/vendor/autoload.php';
+	function debug($data)
+	{
+		echo '<pre>' . print_r($data, 1) . '</pre>';
+	}
+
+	function offerCase(IGadget $product)
+	{
+		echo "<p>Предлогаем чехол для гаджета {$product->getName()}</p>";
+	}
+
+	$book = new BookProduct("Три мушкетера", 100, 300);
+	$notebook = new NoteBookProduct("Lenovo", 1000, "Intel i7");
+
+	offerCase($notebook);
+
+	debug($book);
+	debug($notebook);
+
+	?>
+
+</body>
+
+</html>
---
+++ 14/vendor/core/interfaces/IGadget.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace core\interfaces;
+
+interface IGadget
+{
+	public function getCase();
+	public function getName();
+}
